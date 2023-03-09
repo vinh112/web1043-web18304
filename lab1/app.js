9a688cfb6404fe50cb7b4089dcbb6194eb672cf0
vinh112 <vinhhqpd08113@fpt.edu.vn>
2023-03-09 12:30:23 +0700

    init

---
+++ lab1/app.js
@@ -0,0 +1,11 @@
+// Nhập vào 2 số nguyên a, b từ bàn phím
+let a = parseInt(prompt("Nhập số nguyên a: "));
+let b = parseInt(prompt("Nhập số nguyên b: "));
+
+// Tính tổng c của a và b
+let c = a + b;
+
+// In kết quả bằng document.write và console.log
+document.write("Tổng của a và b là: " + c + "<br>");
+console.log("Tổng của a và b là: " + c);
+
